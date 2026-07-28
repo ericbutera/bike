47d223989271635da2a191383f018c3841923b92
Eric Butera <eric@ericbutera.com>
2026-07-28 10:44:04 -0400

    ci: kaleido crate

--- .woodpecker/test-ui.sh
+++ .woodpecker/test-ui.sh
@@ -2,12 +2,6 @@
 set -e
 npm install -g pnpm@9
 
-KALEIDO_DIR="${KALEIDO_DIR:-kaleido}"
-if [ ! -d "$KALEIDO_DIR/typescript/packages/kaleido" ]; then
-  KALEIDO_DIR="../kaleido"
-fi
-
-(cd "$KALEIDO_DIR/typescript/packages/kaleido" && pnpm install --no-frozen-lockfile)
 cd ui-next
 pnpm install --frozen-lockfile
 pnpm run typecheck
