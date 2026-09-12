91fd5cd047ef936e47b6635cf513107447f7f615
Eric Butera <eric@ericbutera.com>
2026-09-12 08:08:43 -0400

    chore: remove unused garmin experiment

--- garmin-iq/BikeSegments/.vscode/keepalive.js
+++
@@ -1,5 +0,0 @@
-// Keepalive used by the "Build+Install+Run BikeSegments (Debug)" launch config.
-console.log('BikeSegments keepalive started. PreLaunchTask will build and run the PRG.');
-console.log('Press Stop in VS Code to end the session.');
-// Keep process alive
-setInterval(function() {}, 1000 * 60 * 60);
